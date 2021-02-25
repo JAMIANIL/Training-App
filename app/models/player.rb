b39123a2a8e246497930613d7490e073db8c7b29
JAMIANIL <anil@rently.com>
2021-02-25 11:56:35 +0530

    activeadminadded

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,22 +1,19 @@
 class Player < ApplicationRecord
-    #validates :name, presence :true
     belongs_to :team
     belongs_to :country
-    validates :name, uniqueness: true
-    validates :name, length: { minimum: 3 }
+    validates :name, presence: true
+    validates :name, length: { minimum: 2 }
     validates :matches, numericality: { greater_than_or_equal_to: 0 }
 
     before_validation :normalize_name, on: [:create,:edit]
 
+    def calculate_age
+      return Time.now.year-self.dob.year
+    end
+
     private
     def normalize_name 
       self.name = name.downcase.titleize
-    end
-
-    def calculate_age
-      return Time.zone.now.year-self.dob.year
     end 
-
-    public :calculate_age
     
 end
